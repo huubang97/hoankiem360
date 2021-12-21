7477c2a7cbb0399ab7e0fc416c9f4bcb6a171e4a
Duong Vinh <duongvinhmicrosoft@gmail.com>
2021-12-21 17:34:05 +0700

    update

---
+++ Theme/hoankiem360/search.php
@@ -0,0 +1,13 @@
+<?php getHeader() ?>
+<div class="container">
+	<div class="row">
+		<div class="col-12 wr-search">
+			<form action="">
+				<label for="lb-search">Tìm kiếm</label>
+				<input type="text" id="lb-search">
+				<button type="submit">Tìm kiếm</button>
+			</form>
+		</div>
+	</div>
+</div>
+<?php getFooter() ?>
