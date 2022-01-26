3011e7a8594172417719ce7970e6da21fbb7af81
huubang97 <69882867+huubang97@users.noreply.github.com>
2022-01-26 11:37:10 +0700

    Update function.php

--- Plugin/hoankiem360/function.php
+++ Plugin/hoankiem360/function.php
@@ -318,5 +318,52 @@ function getFindnear(){
         }
         return $listData;
 
-}                                       
+}   
+function sendMessageNotifi($data,$target,$id='',$type='manager')
+{
+    /*  
+    Parameter Example
+        $data = array('post_id'=>'12345','post_title'=>'A Blog post');
+        $target = 'single tocken id or topic name';
+        or
+        $target = array('token1','token2','...'); // up to 1000 in one request
+    */
+    //FCM api URL
+    $url = 'https://fcm.googleapis.com/fcm/send';
+    
+    //api_key available in Firebase Console -> Project Settings -> CLOUD MESSAGING -> Server key
+    $server_key = 'AAAAavJLItE:APA91bFaigY2VJiPqtwAI-i8bVpFssYwvyf9CJs_iK-13x5sNBwrISHZBKRuuiDBkoQO5jZJ4oMyyr5YIPr8Flwsv2BdCw9sKb5ISRmBj9ogujAXpgXtGFNh1l4k4VuBxOakAD2h2BWd';
+
+    //$sender_ID = '1039186152782';
+
+    $fields = array();
+    $fields['data'] = $data;
+    $fields['notification'] = $data;
+    if(is_array($target)){
+        $fields['registration_ids'] = $target;
+    }else{
+        $fields['to'] = $target;
+    }
+
+    //header with content_type api key
+    $headers = array(
+        'Content-Type:application/json',
+        'Authorization:key='.$server_key
+    );
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+    $result = curl_exec($ch);
+    if ($result === FALSE) {
+    //die('FCM Send Error: ' . curl_error($ch));
+    }
+    curl_close($ch);
+    return $result;
+}                                    
 ?>
